47daef117181670da91a2d11e325658137aad0c8
Bravudo <gcursino.souza@gmail.com>
2025-06-20 00:06:52 -0300

    Fix: Header

--- server.js
+++ server.js
@@ -11,13 +11,23 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Configuração do EJS e layouts    
+const expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts);
+app.set('layout', './layouts/main'); // Corrigido!
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
 //Routes
 app.use('/filmes', filmesRoutes);
 
 //Rota Inicial
 app.get('/', (req, res) => {
     res.redirect('/filmes')
-})
+})    
 
 // BD
 mongoose.connect(process.env.MONGO_URI)
@@ -27,16 +37,7 @@ mongoose.connect(process.env.MONGO_URI)
         const PORT = process.env.PORT || 3000;
         app.listen(PORT, () => {
             console.log(`Rodando na Porta: ${PORT}`);
-        });
-    })
+        });    
+    })    
     .catch(err => console.log(err));
 
-// Configuração do EJS e layouts
-const expressLayouts = require('express-ejs-layouts');
-app.use(expressLayouts);
-app.set('layout', './layouts/main'); // Corrigido!
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Configurar EJS e layouts
